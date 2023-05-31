066558cdf6b3f32d1f3fc16baf17dcc844cd8f8a
P05h <121305788+P05h@users.noreply.github.com>
2023-05-31 18:21:33 -0400

    Update uv.bundle.js

--- uv/uv.bundle.js
+++ uv/uv.bundle.js
@@ -35154,7 +35154,7 @@ __webpack_require__.r(__webpack_exports__);
 const xor = {
     encode(str){
         if (!str) return str;
-        return encodeURIComponent(str.replace("https://ixl.com/"), "/");
+        return encodeURIComponent(str.replace("https://ixl.com"), "/");
     },
     decode(str){
         if (!str) return str;
